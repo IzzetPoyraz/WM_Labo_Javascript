5ea7dc3bd004b26696649850a6253f3bf21beab5
Izzet <izzet9719@gmail.com>
2023-03-29 17:06:26 +0200

    update

--- pyramides.js
+++ pyramides.js
@@ -2,8 +2,22 @@
 
 (function () {
 
+    const pyramide = document.getElementById('input-piramids');
+    const btn = document.getElementById('btn-piramids');
+    let string = "";
 
-    
+    btn.addEventListener('click', () => {
+        string = "";
+        for (let i = 1; i <= pyramide; i++) {
+            for (let j = 0; j < i; j++) {
+                string += "*";
+            }
+            string += "\n";
+        }
+    });
 
+
+    console.log(string);
 
 }());
+
